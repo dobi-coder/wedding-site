075c9a53e7e5228f8f3521310f7d50e7ef932884
Maxime Chauveau <maximechauveau@Maximes-MacBook-Air.local>
2026-08-28 13:58:45 +0200

    Issue #5 - Update repo to use GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    site: "https://dobi-coder.github.io",
+    base: "/wedding-site",
 	vite: {
 		plugins: [tailwindcss()],
 	},
